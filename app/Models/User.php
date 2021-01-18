d8ad169bc5f323c631a93412b90c637c9510db64
Cesare Bek <cesarebek1@gmail.com>
2021-01-18 16:28:16 +0100

    2. Created Task model and  migrated the files to db

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+//Namespace added for HasApiTokens
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens ,HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -40,4 +42,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //Function that associate tasks to his owner
+    public function task(){
+        return $this->hasMany('App\Models\Task');
+    }
 }
---
+++ database/migrations/2021_01_18_141736_create_tasks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("description")->nullable();
+            $table->boolean("completed")->default(false);
+            $table->timestamp("completed_at")->nullable();
+            $table->timestamp("due_to")->nullable();
+            //Wiring user to his task
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")->references("id")->on("users")->onDelete("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
