868ff24ff0fe7df209574de7adf7f554c3a5aee9
Cesare Bek <cesarebek1@gmail.com>
2021-01-19 10:26:32 +0100

    6. Customized auth middleware

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -36,5 +37,26 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+
+	// Here you can return your own response or work with request
+	// return response()->json(['status' : false], 401);
+
+	// This is the default
+         if(!$request->expectsJson()){
+            return response()->json([
+                'message' => $exception->getMessage(),
+            ], 401);
+        }
     }
 }
